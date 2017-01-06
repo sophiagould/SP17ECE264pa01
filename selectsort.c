89a781db5a3928d91b7a27d7c6043cbb71c702e8
Milind Kulkarni <milind@purdue.edu>
2017-01-06 09:17:19 -0500

    source files for PA01

---
+++ selectsort.c
@@ -0,0 +1,17 @@
+#ifdef SELECTION_SORT
+#include "pa01.h"
+void selectSort(int * arr, int size)
+//arr : the array to be sorted
+//size : the number of elements in the array
+
+// In each iteration, the number of elements to be sorted shrinks by one
+// i.e., one element is at the correct position.
+//
+// Among the remaining elements, find the smallest value and put it 
+// before the other unsorted elements
+{
+
+	//TODO: Fill this in to complete PA01.
+
+}
+#endif
