307790ecdffe36db3785d1943baa8b68f2c755db
Sophia Alexandra Gould <gould7@ecegrid-thin1.ecn.purdue.edu>
2017-01-17 18:50:03 -0500

    first commit

--- selectsort.c
+++ selectsort.c
@@ -1,17 +1,31 @@
 #ifdef SELECTION_SORT
 #include "pa01.h"
 void selectSort(int * arr, int size)
-//arr : the array to be sorted
-//size : the number of elements in the array
+	//arr : the array to be sorted
+	//size : the number of elements in the array
 
-// In each iteration, the number of elements to be sorted shrinks by one
-// i.e., one element is at the correct position.
-//
-// Among the remaining elements, find the smallest value and put it 
-// before the other unsorted elements
+	// In each iteration, the number of elements to be sorted shrinks by one
+	// i.e., one element is at the correct position.
+	//
+	// Among the remaining elements, find the smallest value and put it 
+	// before the other unsorted elements
 {
-
-	//TODO: Fill this in to complete PA01.
-
+	int least;
+	int new;
+	for (int i = 0; i < size; i++)
+	{
+		least = i;
+		for(int n = i + 1; n <= size; n++)
+		{
+			if(arr[n] < arr[least])
+			{
+				least = n;
+			}
+		}
+		new = arr[i];
+		arr[i] = arr[least];
+		arr[least] = new;
+	}
+	return;
 }
 #endif
